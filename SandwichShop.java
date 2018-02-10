d3f45b8e7f877c9ea73fa65005d41425eb9a5535
WhitneyVillmer <35549399+WhitneyVillmer@users.noreply.github.com>
2018-02-09 23:27:08 -0600

    Add files via upload
    Here is an updated version of the Sandwich Shop project with revisions as suggested by my mentor.

--- SandwichShop.java
+++ SandwichShop.java
@@ -1,6 +1,6 @@
 package com.company;
-
 import java.util.Scanner;
+
 public class SandwichShop {
 
     public static void main(String[] args) {
@@ -9,58 +9,82 @@ public class SandwichShop {
         int goalForVeggies = 50;
         int goalForBurgers = 250;
         int goalForSubs = 180;
-        int goalForSoup = 70;
-
+        int goalForSoups = 70;
 
         System.out.println("The sales goal for veggie sandwiches is 50.");
         System.out.println("How many veggie sandwiches were sold today? ");
         int veggieSandwichesSold = keyboard.nextInt();
-        if (veggieSandwichesSold >= goalForVeggies) {
-            System.out.println("Met goal!");
-        } else {
-            System.out.println("Fell short");
+        int closeToVeggiesGoal = 40;
+        if (veggieSandwichesSold >= closeToVeggiesGoal
+                && veggieSandwichesSold < goalForVeggies) {
+
+            System.out.println("Close but did not reach the goal.");
         }
-        keyboard.skip("\n");
+            else if (veggieSandwichesSold <= closeToVeggiesGoal) {
+            System.out.println("You did not meet today's goal.");
+        }
+        else if (veggieSandwichesSold >= goalForVeggies) {
+            System.out.println("You met today's goal!");
+        }
 
         System.out.println("The sales goal for burgers is 250.");
         System.out.println("How many burgers were sold today? ");
         int burgersSold = keyboard.nextInt();
-        if (burgersSold >= goalForBurgers) {
-            System.out.println("Met goal!");
-        } else {
-            System.out.println("Fell short");
-        }
-        keyboard.skip("\n");
+        int closeToBurgersGoal = 200;
+        if (burgersSold >= closeToBurgersGoal
+                && burgersSold < goalForBurgers) {
 
-        System.out.println("The sales goal for subs is 180.");
-        System.out.println("How many subs were sold today? ");
-        int subsSold = keyboard.nextInt();
-        if (subsSold >= goalForSubs) {
-            System.out.println("Met goal!");
-        } else {
-            System.out.println("fell short");
+            System.out.println("Close but did not reach the goal.");
         }
-        keyboard.skip("\n");
+        else if (burgersSold <= closeToBurgersGoal) {
+            System.out.println("You did not meet today's goal.");
+        }
+        else if (burgersSold >= goalForBurgers) {
+            System.out.println("You met today's goal!");
+        }
+
+        System.out.println("The sales goal for sub sandwiches is 180.");
+        System.out.println("How many sub sandwiches were sold today? ");
+        int subSandwichesSold = keyboard.nextInt();
+        int closeToSubGoal = 144;
+        if (subSandwichesSold >= closeToSubGoal
+                && subSandwichesSold < goalForSubs) {
+
+            System.out.println("Close but did not reach the goal.");
+        }
+        else if (subSandwichesSold <= closeToSubGoal) {
+            System.out.println("You did not meet today's goal.");
+        }
+        else if (subSandwichesSold >= goalForSubs) {
+            System.out.println("You met today's goal!");
+        }
 
         System.out.println("The sales goal for soups is 70.");
         System.out.println("How many soups were sold today? ");
         int soupSold = keyboard.nextInt();
-        if (soupSold >= goalForSoup) {
-            System.out.println("Met goal!");
-        } else {
-            System.out.println("Fell short");
+        int closeToSoupGoal = 56;
+        if (soupSold >= closeToSoupGoal
+                && soupSold < goalForSoups) {
+
+            System.out.println("Close but did not reach the goal.");
         }
-        keyboard.skip("\n");
-
-        int total = (veggieSandwichesSold + burgersSold + subsSold + soupSold);
-        int goal = (goalForVeggies + goalForBurgers + goalForSubs + goalForSoup);
-
-        if (total >= goal) {
-            System.out.println("Made goal for everything!");
-
-        } else {
-            System.out.println("Goal for everything was not met");
+        else if (soupSold <= closeToSoupGoal) {
+            System.out.println("You did not meet today's goal.");
         }
-    }
+        else if (soupSold >= goalForSoups) {
+            System.out.println("You met today's goal!");
+        }
 
-}
+
+        if (veggieSandwichesSold >= goalForVeggies &&
+                burgersSold >= goalForBurgers &&
+                subSandwichesSold >= goalForSubs &&
+                soupSold >= goalForSoups) {
+            System.out.println("You met your goal for all the items!");}
+            else{
+                System.out.println("You did not meet the goal on all of your items.");
+            }
+            }
+        }
+
+
